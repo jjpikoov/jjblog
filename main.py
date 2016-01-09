258c0fa8d1b110bee80f6a4c2e2941a7b0917f0a
Pawel Jankowski <jjpikoov@gmail.com>
2016-01-09 12:16:41 +0100

    Admin stub created, config added

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-import flask
+import sqlite3
+from flask import Flask, g
+from contextlib import closing
+from admin import admin
 
-class foo(object):
+app = Flask(__name__)
+app.config.from_object('config.DevConfig')
 
-sdfkjskdf    """Docstring for foo. """
 
-    def __init__(self):
-        """foo """
-        print(flask.appcontext_tearing_down)
-        lsdkfjsdkljf
-        sdklj
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/')
+def show_blog():
+    return "foo"
+
+app.register_blueprint(admin, url_prefix='/admin/')
+
+if __name__ == '__main__':
+    app.run()
